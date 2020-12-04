fbae746432d33b4e21532e8006e61fbb8b7883c5
RysnikM <m.rysnik.42@gmail.com>
2020-12-04 13:43:35 +0300

    init branch server001

--- src/public_html/start/oee/oee_otcet/assets/js/tooltip.js
+++
@@ -1,3 +0,0 @@
-$(function () {
-$('.material-tooltip-main').tooltip({template: '<div class="tooltip md-tooltip"><div class="tooltip-arrow md-arrow"></div><div class="tooltip-inner md-inner"></div></div>'});
-});
--- src/public_html/start/vibration/online_v/vibroOnline/assets/js/check.js
+++
@@ -1,9 +0,0 @@
-$('.material-icons').click(function(){
-	if ($(this).text()=='check_box'){
-		$(this).text('check_box_outline_blank');
-	}
-	else{
-		$(this).text('check_box');
-	}
-	
-})
